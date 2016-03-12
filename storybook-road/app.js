39c1c0ed2f122a04eb8ceef90ed238c6d8aaf88d
Jeremy D <jeremy.dormitzer@gmail.com>
2016-03-12 16:56:28 -0700

    started implenting login controller

--- storybook-road/app.js
+++ storybook-road/app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var assert = require('assert');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var db = require('./db'); //the database
+
+var routes = require('./controllers/index');
 
 var app = express();
 
@@ -23,7 +25,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+
+//connect to database
+db.connect(function(err) {
+	assert.equal(null, err);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- storybook-road/controllers/index.js
+++ storybook-road/controllers/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var assert = require('assert');
+
+//handle routing
+router.use('/login', require('./login-ctrl'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
