7f97640446feb4e6c025ab2f0cdc9176965dfa36
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-03-20 19:41:07 -0600

    removed console logging in auth.js

--- storybook-road/middlewares/auth.js
+++ storybook-road/middlewares/auth.js
@@ -1,6 +1,5 @@
 //ensure that there is a user stored in the session to prevent unauthorized access to pages
 module.exports = function(req, res, next) {
-	console.log(req.session);
 	if (req.session.user) {
 		console.log('authorized user');
 		//user is authorized, proceed to next route
