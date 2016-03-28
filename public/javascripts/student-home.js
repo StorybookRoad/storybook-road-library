6db8b37fb518dfb6bb0a6674457f99ed9cc0994c
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-03-28 01:46:51 -0600

    styled student home

--- public/javascripts/student-home.js
+++ public/javascripts/student-home.js
@@ -1,11 +1,12 @@
 var selected_story = null;
 
 $(document).ready(function () {
-	$('#new_story_form').hide();
 	update_story_list();
 
 	//handle story selection
 	$(document).on('click', '.story_item', function (event) {
+		$('.story_item').removeClass('active');
+		$(this).addClass('active');
 		selected_story = $(this).data('id');
 		$('#continue_button').prop('disabled', false);
 	});
@@ -24,14 +25,8 @@ $(document).ready(function () {
 				var theme = result[item];
 				$('#themes').append($('<option>', {value: item, text: theme.name}));
 			}
-			$('#new_story_form').show();
+			$('#new_story_modal').modal();
 		});
-	});
-
-	//cancel button for story form
-	$('#cancel_new_story').click(function (event) {
-		$('#themes').empty();
-		$('#new_story_form').hide();
 	});
 });
 
@@ -45,11 +40,20 @@ function update_story_list() {
 		else {
 			for (item in result) {
 				var story = result[item];
-				$('#story_list').append($('<li>', {
-					text: story.words.character + "'s Story - Progress: " + story.progress,
-					class: "story_item",
+				$('#story_list').append($('<a>', {
+					href: '#',
+					class: "story_item list-group-item",
 					"data-id": story._id
-				}));
+				}).append(
+					$('<h4>', {
+						text: story.words.character + "'s Story",
+						class: 'list-group-item-heading'
+					}), ($('<p>', {
+							text: "Completed " + story.progress + " out of " + story.phrases.length + " puzzles.",
+							class: "list-group-item-text"
+						})
+					)
+				));
 			}
 		}
 	});
