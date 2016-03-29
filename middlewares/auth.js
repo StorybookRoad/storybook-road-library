e8c072788c795c22dc2390a9b22e6a2803b66a15
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-03-28 21:37:45 -0600

    modified auth.js to correctly check for session.user

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,10 @@
 //ensure that there is a user stored in the session to prevent unauthorized access to pages
 
 //authorize teachers
-exports.authTeacher = function(req, res, next) {
+exports.authTeacher = function (req, res, next) {
+	// first check if user exists
+	if (!req.session.user) res.status(401).render('401');
+
 	if (req.session.user.role == 'teacher') {
 		//user is authorized, proceed to next route
 		next();
@@ -14,6 +17,9 @@ exports.authTeacher = function(req, res, next) {
 
 //authorize students
 exports.authStudent = function (req, res, next) {
+	//first check if user exists
+	if (!req.session.user) res.status(401).render('401');
+
 	if (req.session.user.role == 'student') {
 		//user is authorized, proceed to next route
 		next();
@@ -23,3 +29,13 @@ exports.authStudent = function (req, res, next) {
 		res.status(401).render('401');
 	}
 };
+
+//helper function to check for session.user
+var auth = function (req, res, next) {
+	if (req.session.user) {
+		next();
+	}
+	else {
+		res.status(401).render('401');
+	}
+}
